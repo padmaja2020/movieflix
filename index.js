1aaa2d51d0a290dd9149094eec988742b5848585
Padmaja <pvemparala@hotmail.com>
2020-10-08 21:16:08 -0700

    connect to mongodb atlas

--- index.js
+++ index.js
@@ -15,10 +15,16 @@ app.use(cors());
 const { check, validationResult } = require("express-validator");
 
 //Connect to the mongo db
-mongoose.connect("mongodb://localhost:27017/test", {
+
+mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+// mongoose.connect("mongodb://localhost:27017/test", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
 app.use(bodyParser.json());
 //Logging using Morgan
