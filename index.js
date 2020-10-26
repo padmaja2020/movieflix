94ba0ab02162d60fd03718d4620a51b81a067557
Padmaja <pvemparala@hotmail.com>
2020-10-25 19:25:14 -0700

    enable cors for local react app/all domain

--- index.js
+++ index.js
@@ -11,7 +11,19 @@ require("./passport.js");
 const Movies = Models.Movie;
 const Users = Models.User;
 const cors = require("cors");
-app.use(cors());
+//app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 const { check, validationResult } = require("express-validator");
 
 //Connect to the mongo db
