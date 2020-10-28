b1b594ab31a5d61c07b335b90c2b773345395298
Padmaja <pvemparala@hotmail.com>
2020-10-27 17:23:02 -0700

    enable cors for local react app/all domain

--- index.js
+++ index.js
@@ -10,22 +10,22 @@ const passport = require("passport");
 require("./passport.js");
 const Movies = Models.Movie;
 const Users = Models.User;
-//const cors = require("cors");
-//app.use(cors());
-//let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://padmaja-myflix.herokuapp.com',];
+const cors = require("cors");
+app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://padmaja-myflix.herokuapp.com',];
 
-//app.use(cors({                                                             
+app.use(cors({                                                             
   
-//   origin: (origin, callback) => {
-//     if(!origin) return callback(null, true);
-//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-//       return callback(new Error(message ), false);
-//     }
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
   
-//     return callback(null, true);
-//   }
-// }));
+    return callback(null, true);
+  }
+}));
 const { check, validationResult } = require("express-validator");
 let auth = require("./auth")(app);
 //Connect to the mongo db
