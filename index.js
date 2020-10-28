a813773605a18b7c4861641fd6f66d5d8e297ea9
Padmaja <pvemparala@hotmail.com>
2020-10-27 21:43:45 -0700

    enable cors for local react app/all domain

--- index.js
+++ index.js
@@ -1,49 +1,42 @@
+const express = require('express'),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose'),
+  Models = require('./models.js'),
+  morgan = require('morgan');
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const Models = require("./models.js");
-const morgan = require("morgan");
-const uuid = require("uuid");
-
-const passport = require("passport");
-require("./passport.js");
+const passport = require('passport');
+require('./passport');
 
 const app = express();
 const Movies = Models.Movie;
 const Users = Models.User;
 
-//Connect to the mongo db
+// Local connection
+// mongoose.connect('mongodb://localhost:27017/victorvilleDB', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
+// Connection to Remote DB on MongoDBAtlas
 mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 mongoose.set('useFindAndModify', false);
 
-const { check, validationResult } = require("express-validator");
-const cors = require("cors");
+const { check, validationResult } = require('express-validator');
+
+const cors = require('cors');
 app.use(cors());
 
+/*
+CORS - Allowed origins/domains
+*/
 var allowedOrigins = [
   'http://localhost:8080',
   'http://localhost:1234',
   'https://padmaja-myflix.herokuapp.com',
 ];
-//var allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://padmaja-myflix.herokuapp.com',];
-// CORS - Allowed origins/domains
-// app.use(cors({                                                             
-  
-//   origin: (origin, callback) => {
-//     if(!origin) return callback(null, true);
-//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-//       return callback(new Error(message ), false);
-//     }
-  
-//     return callback(null, true);
-//   }
-// }));
 
 app.use(
   cors({
@@ -61,18 +54,80 @@ app.use(
   })
 );
 
-//Middleware
-//Logging using Morgan
-app.use(morgan("common"));
+// Midddleware
+app.use(morgan('common'));
 app.use(bodyParser.json());
+var auth = require('./auth')(app);
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
-let auth = require("./auth")(app);
-// app.use(function (err, req, res, next) {
-//   console.error(err.stack);
-//   res.status(500).send('Something broke!');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const uuid = require("uuid");
+
+
+
+
+
+// let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://padmaja-myflix.herokuapp.com' ,];
+
+// app.use(cors({                                                             
+  
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//       return callback(new Error(message ), false);
+//     }
+  
+//     return callback(null, true);
+//   }
+// }));
+
+
+
+//let auth = require("./auth")(app);
+//Connect to the mongo db
+
+// mongoose.connect(process.env.CONNECTION_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
 // });
 
+// mongoose.connect("mongodb://localhost:27017/test", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
+//app.use(bodyParser.json());
+//Logging using Morgan
+
+//app.use(morgan("common"));
 
 //request to GET a list of all the movies
 
@@ -371,22 +426,3 @@ const port = process.env.PORT || 8080;
 app.listen(port, "0.0.0.0", (req, res) => {
   console.log("Listening to port" + port);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
