cc145fccf2c63307d526e356261e22f41ed629ff
Padmaja <pvemparala@hotmail.com>
2020-10-27 21:40:50 -0700

    enable cors for local react app/all domain

--- index.js
+++ index.js
@@ -25,41 +25,41 @@ const { check, validationResult } = require("express-validator");
 const cors = require("cors");
 app.use(cors());
 
-// var allowedOrigins = [
-//   'http://localhost:8080',
-//   'http://localhost:1234',
-//   'https://padmaja-myflix.herokuapp.com',
-// ];
-var allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://padmaja-myflix.herokuapp.com',];
+var allowedOrigins = [
+  'http://localhost:8080',
+  'http://localhost:1234',
+  'https://padmaja-myflix.herokuapp.com',
+];
+//var allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://padmaja-myflix.herokuapp.com',];
 // CORS - Allowed origins/domains
-app.use(cors({                                                             
+// app.use(cors({                                                             
   
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//       return callback(new Error(message ), false);
+//     }
   
-    return callback(null, true);
-  }
-}));
+//     return callback(null, true);
+//   }
+// }));
 
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       if (!origin) return callback(null, true);
-//       if (allowedOrigins.indexOf(origin) === -1) {
-//         // If a specific origin isn’t found on the list of allowed origins
-//         var message =
-//           'The CORS policy for this application doesn’t allow access from origin ' +
-//           origin;
-//         return callback(new Error(message), false);
-//       }
-//       return callback(null, true);
-//     },
-//   })
-// );
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        var message =
+          'The CORS policy for this application doesn’t allow access from origin ' +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 //Middleware
 //Logging using Morgan
